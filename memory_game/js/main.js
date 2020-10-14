9e76cc66c05805d43e5253b09a47c3bb08bd25a9
sara limbu <iamsebi7@gmail.com>
2020-10-14 19:19:00 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,44 +1,58 @@
-const cards = [
+var cards = [
   {
-    rank: 'queen',
-    suit: 'hearts',
-    cardImage: 'images/queen-of-hearts.png'
+  rank: 'queen',
+  suit: 'hearts',
+  cardImage: 'images/queen-of-hearts.png'
   },
   {
-    rank: 'queen',
-    suit: 'diamonds',
-    cardImage: 'images/queen-of-diamonds.png'
+  rank: 'queen',
+  suit: 'diamonds',
+  cardImage: 'images/queen-of-diamonds.png'
   },
   {
-    rank: 'king',
-    suit: 'hearts',
-    cardImage: 'images/king-of-hearts.png'
+  rank: 'king',
+  suit: 'hearts',
+  cardImage: 'images/king-of-hearts.png'
   },
   {
-    rank: 'king',
-    suit: 'diamonds',
-    cardImage: 'images/king-of-diamonds.png'
+  rank: 'king',
+  suit: 'diamonds',
+  cardImage: 'images/king-of-diamonds.png'
   }
 ];
-let cardsInPlay = [];
+var cardsInPlay = [];
+var gameBoardDiv = document.getElementById('game-board');
 
 function checkForMatch() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("Success, you found the match!");
+  alert("Success, you found the match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 }
-
-function flipCard(cardId) {
+  
+function flipCard() {
+  let cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+  this.setAttribute('src',cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement =  document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+    gameBoardDiv.appendChild(cardElement);
+  }
+}
+  
+createBoard();
+  
+  
